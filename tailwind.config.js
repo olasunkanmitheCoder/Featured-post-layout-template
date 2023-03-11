4d5a7c5e117dc57ca2685018a03bf5824f677613
olasunkanmitheCoder <olasunkanmidaniel003@gmail.com>
2023-03-11 01:26:43 +0100

    featured post layout template

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./build/*html"
+  ],
+  theme: {
+    fontFamily: {
+      serif: ['Playfair Display', 'serif'],
+      sans: ['Open Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
